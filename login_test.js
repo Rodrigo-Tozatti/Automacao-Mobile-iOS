fe50f104bca653e0991c01512881dca847f97269
Rodrigo Rosseti Tozatti <rodrigo.tozatti@hotmail.com>
2024-09-30 23:00:17 -0300

    Cenário 01Cenário 01: Deve realizar login com sucesso

--- login_test.js
+++ login_test.js
@@ -1,6 +1,15 @@
 Feature('login');
 
 Scenario('Cenário 01: Deve realizar login com sucesso',  ({ I }) => {
+    
+    //preencher campos
     I.fillField('~email', 'teste@teste.com')
+    I.fillField('~senha', '123456')
 
+    //clicar botão Entrar
+    I.click('~entrar')
+
+    //checar
+    I.waitForElement('~salvar', 3)
+    I.seeElement('~salvar')
 });
